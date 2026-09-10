489a66a5f60a061ff6f50a2ee99a1f89f0dda074
Judson_Alves <judson.fontes@aluno.senai.br>
2026-09-10 11:34:55 -0300

    MVC S11 dia 3 finalizado

--- src/routes/tarefas.routes.js
+++ src/routes/tarefas.routes.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
+
+const validar = require('../middlewares/validar');
+const schemas = require('../middlewares/schemas');
 const tarefasController = require('../controllers/tarefas.controller');
 
 router.get('/estatisticas', tarefasController.estatisticas);
 router.get('/resumo', tarefasController.resumo)
 
 router.get('/', tarefasController.listar);
-router.post('/', tarefasController.criar);
-router.put('/:id', tarefasController.atualizar);
+router.post('/', validar(schemas.usuario), usuariosController.criar);
+router.put('/:id', validar(schemas.usuario), usuariosController.atualizar);
 router.delete('/:id', tarefasController.remover);
 router.get('/:id', tarefasController.buscarPorId);
 
--- utils/src/middlewares/temporizador.js
+++
@@ -1,7 +0,0 @@
-function temporizador(req, res, next) {
-    const inicio = Date.now();
-    next();
-    const fim = Date.now();
-    console.log(`Tempo de execução: ${fim - inicio} ms`);
-}
-module.exports = temporizador;
