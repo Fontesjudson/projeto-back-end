653169229deb81d36d6dec8e67fba9d9f468fa89
Judson_Alves <judson.fontes@aluno.senai.br>
2026-09-23 09:54:53 -0300

    cors corrigido

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 const PORTA = 3001;
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || "*",//'http://localhost:5173',
+    origin: '*',//process.env.CORS_ORIGIN || 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeader: ['Content-Type', 'Authorization'],
     
